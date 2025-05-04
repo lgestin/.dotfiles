0f56c8dfdb76d64b3d829d803345431333f2fe53
lgestin <lucas.gestin.pro@gmail.com>
2025-05-04 19:06:28 +0200

    install p10k

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ sudo apt install stow
 brew install stow
 ```
 
-# Install nvim
+# Install packages
 
 
 ```sh
--- scripts/omz-setup.sh
+++ scripts/omz-setup.sh
@@ -69,4 +69,7 @@ else
 fi
 
 echo -e "${GREEN}Custom Oh My Zsh plugins installed!${NC}"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
 echo -e "${GREEN}Setup complete!${NC}"
